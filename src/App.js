7afabe7a4983e0877fd69fd2a1c1f92c7cacd726
Kirill Granin <krllgranin@gmail.com>
2020-11-12 22:30:03 +0300

    32. Creating a Functional Component
    Create a Person.js component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import Person from './Person/Person'
 
 function App() {
   return (
     <div className="App">
       <h1>Hi, I am react app</h1>
       <p>This is really working!</p>
+      <Person />
     </div>
   );
 }
