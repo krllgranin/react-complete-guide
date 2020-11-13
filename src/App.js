87a5c152e307ba0ceb01f86003f8a4e45a83e18a
Kirill Granin <krllgranin@gmail.com>
2020-11-13 22:00:32 +0300

    36. Working with Props
    Create props name and age in the Person.js

--- src/App.js
+++ src/App.js
@@ -6,9 +6,9 @@ function App() {
     <div className="App">
       <h1>Hi, I am react app</h1>
       <p>This is really working!</p>
-      <Person />
-      <Person />
-      <Person />
+      <Person name="Max" age="28" />
+      <Person name="Manu" age="29" >My hobbies: Racing</Person>
+      <Person name="Stephanie" age="26" />
     </div>
   );
 }
