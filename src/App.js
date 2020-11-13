b980858723cf2274721d5fea01858dbec44b7a2e
Kirill Granin <krllgranin@gmail.com>
2020-11-13 22:15:50 +0300

    38. Understanding & Using State
    Create state for App

--- src/App.js
+++ src/App.js
@@ -2,13 +2,21 @@ import './App.css';
 import Person from './Person/Person'
 
 function App() {
+  const state = {
+    persons: [
+      {name: 'Max', age: 28},
+      {name: 'Manu', age: 29},
+      {name: 'Stephanie', age: 26}
+    ]
+  }
+
   return (
     <div className="App">
       <h1>Hi, I am react app</h1>
       <p>This is really working!</p>
-      <Person name="Max" age="28" />
-      <Person name="Manu" age="29" >My hobbies: Racing</Person>
-      <Person name="Stephanie" age="26" />
+      <Person name={state.persons[0].name} age={state.persons[0].age} />
+      <Person name={state.persons[1].name} age={state.persons[1].age} >My hobbies: Racing</Person>
+      <Person name={state.persons[2].name} age={state.persons[2].age} />
     </div>
   );
 }
