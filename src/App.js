cfbc48f9a5aa43b2173722ad5f9396e18996bd17
Kirill Granin <krllgranin@gmail.com>
2020-11-17 22:05:45 +0300

    40. Handing Events with Methods
    Create "switchNameHandler" and add onClick event to the button

--- src/App.js
+++ src/App.js
@@ -10,13 +10,18 @@ function App() {
     ]
   }
 
+  const switchNameHandler = () => {
+    console.log('Was clicked!')
+  }
+
   return (
     <div className="App">
       <h1>Hi, I am react app</h1>
       <p>This is really working!</p>
-      <Person name={state.persons[0].name} age={state.persons[0].age} />
-      <Person name={state.persons[1].name} age={state.persons[1].age} >My hobbies: Racing</Person>
-      <Person name={state.persons[2].name} age={state.persons[2].age} />
+      <button onClick={switchNameHandler}>Switch name</button>
+      <Person name={state.persons[0].name} age={state.persons[0].age}/>
+      <Person name={state.persons[1].name} age={state.persons[1].age}>My hobbies: Racing</Person>
+      <Person name={state.persons[2].name} age={state.persons[2].age}/>
     </div>
   );
 }
